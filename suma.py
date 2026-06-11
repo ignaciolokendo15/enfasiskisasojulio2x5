82db9b78d5acf20de45c8d610f736bb40c595c6c
ignaciolokendo15 <i.perezgomez1504@gmail.com>
2026-06-11 14:45:43 -0400

    calculrdorrr
    ckarilhadro ade finasio

---
+++ suma.py
@@ -0,0 +1,9 @@
+def sumar():
+    resultado = v1+v2
+    return resultado 
+
+
+valor1 = int(input("Ingrese valor: "))
+valor2 = int(input("Ingrese valor: "))
+
+print(f"el resultado es: {sumar(valor1,valor2)}")
